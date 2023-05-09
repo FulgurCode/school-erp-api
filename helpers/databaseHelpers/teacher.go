f57ce7828d76dbd5969eaad11ccc550c8340fa92
Vaishakh GK <vaishakhgk2006@gmail.com>
2023-05-09 12:27:35 +0530

    Teacher signup & signup-otp route added

--- helpers/databaseHelpers/teacher.go
+++ helpers/databaseHelpers/teacher.go
@@ -17,3 +17,29 @@ func GetTeacher(teacherId primitive.ObjectID) (map[string]interface{}, error) {
 	var err = db.Collection("teachers").FindOne(context.Background(), bson.M{"_id": teacherId}).Decode(&teacher)
 	return teacher, err
 }
+
+// Getting teacher using email
+func GetTeacherWithEmail(email string) (map[string]interface{}, error) {
+	// database
+	var db = connections.Db
+	// Get teacher from database
+	var teacher map[string]interface{}
+	var err = db.Collection("teachers").FindOne(context.Background(), bson.M{"email": email}).Decode(&teacher)
+	return teacher, err
+}
+
+// Create Teacher user
+func CreateTeacher(teacher map[string]interface{}) error {
+	// database
+	var db = connections.Db
+	var _, err = db.Collection("teachers").InsertOne(context.Background(), teacher)
+	return err
+}
+
+// Create Teacher user
+func UpdateTeacherWithMail(search map[string]interface{}, teacher map[string]interface{}) error {
+	// database
+	var db = connections.Db
+	var _, err = db.Collection("teachers").UpdateOne(context.Background(), search, bson.M{"$set": teacher})
+	return err
+}
